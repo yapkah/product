cb02e645fd4933f04a9da1953f36e4ffe5927af3
kah hou <yapkah@sunway.com.my>
2026-01-20 22:34:29 +0800

    add back missing route

--- product/routes/web.php
+++ product/routes/web.php
@@ -39,5 +39,7 @@ Route::post('/categories', [CategoryController::class, 'store'])->name('categori
 Route::get('/categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
 Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
 Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+Route::post('/categories/bulk-delete', [CategoryController::class, 'bulkDelete'])
+    ->name('categories.bulk-delete');
 
 });
